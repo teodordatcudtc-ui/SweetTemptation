83c8d98747171bd390568da677e7f34ed15288c5
teodordatcudtc-ui <teodordatcu.dtc@gmail.com>
2025-11-17 16:25:58 +0200

    Initial commit - Sweet Temptation

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
         </div>
 
         {/* Decorative Elements */}
-        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-cream to-transparent" />
+        <div className="hidden md:block absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-cream to-transparent" />
       </section>
 
       {/* Signature Desserts Section */}
